1d4de10ec69204331c589a3b20a8ec442f9a4483
Kai Feng Chew <kafechew@gmail.com>
2025-02-12 19:04:49 +0800

    Use dotenv for database connection configuration so that our credentials is not exposed

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL ='postgresql://millaridzuan:Miso2706$@localhost/fastapi_db'
+# Load environment variables
+load_dotenv()
+
+# Get DATABASE_URL from environment variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
